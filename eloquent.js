60c945b792ba272e024ae46d4a5ad96e7fc54fe6
Michael Lowe <mikkeyfluttie@gmail.com>
2016-09-30 12:56:13 +1300

    Add eloquent and index

---
+++ eloquent.js
@@ -0,0 +1,16 @@
+function minimum (x,y){
+  if (x > y) {
+    return x;
+  }
+  else {
+    return y;
+  }
+}
+
+console.log(minimum(0,10));
+console.log(minimum(0,-10));
+
+
+function isEven (x) {
+  return
+}
