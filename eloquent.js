f7c2247e5165a1a55c35575d1007a0f5530683af
Michael Lowe <mikkeyfluttie@gmail.com>
2016-09-30 13:20:04 +1300

    Add isEven

--- eloquent.js
+++ eloquent.js
@@ -1,5 +1,6 @@
+
 function minimum (x,y){
-  if (x > y) {
+  if (x < y) {
     return x;
   }
   else {
@@ -10,7 +11,28 @@ function minimum (x,y){
 console.log(minimum(0,10));
 console.log(minimum(0,-10));
 
+var target = document.getElementById("minimum");
+target.innerHTML = minimum(3,4);
+target.innerHTML += "<br>"+minimum(10,100);
+
+var target = document.getElementById("isEven");
+target.innerHTML = "3 is Even: "+isEven(3);
+target.innerHTML += "<br>4 is Even: "+isEven(4);
+target.innerHTML += "<br>75 is Even: "+isEven(75);
+target.innerHTML += "<br>-1 is Even: "+isEven(-1);
+target.innerHTML += "<br>-100 is Even: "+isEven(-100);
+
+
 
 function isEven (x) {
-  return
+  x = Math.abs(x);
+  if (x === 0){
+    return true;
+  }
+  else if (x === 1){
+    return false;
+  }
+  else {
+    return isEven(x-2);
+  }
 }
