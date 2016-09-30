c5af687a62d17f84aae6663d64a8b93da486b356
Michael Lowe <mikkeyfluttie@gmail.com>
2016-09-30 13:29:46 +1300

    ADD countChar()

--- eloquent.js
+++ eloquent.js
@@ -1,4 +1,3 @@
-
 function minimum (x,y){
   if (x < y) {
     return x;
@@ -7,22 +6,6 @@ function minimum (x,y){
     return y;
   }
 }
-
-console.log(minimum(0,10));
-console.log(minimum(0,-10));
-
-var target = document.getElementById("minimum");
-target.innerHTML = minimum(3,4);
-target.innerHTML += "<br>"+minimum(10,100);
-
-var target = document.getElementById("isEven");
-target.innerHTML = "3 is Even: "+isEven(3);
-target.innerHTML += "<br>4 is Even: "+isEven(4);
-target.innerHTML += "<br>75 is Even: "+isEven(75);
-target.innerHTML += "<br>-1 is Even: "+isEven(-1);
-target.innerHTML += "<br>-100 is Even: "+isEven(-100);
-
-
 
 function isEven (x) {
   x = Math.abs(x);
@@ -38,10 +21,42 @@ function isEven (x) {
 }
 
 function countBs (str){
+  var count = 0;
   for (var i = 0; i < str.length; i++){
     if (str.charAt(i) === "B"){
-      return true;
+      count ++;
     }
   }
-  return false;
+  return count;
 }
+
+function countChar (str, char) {
+  var count = 0;
+  for (var i = 0; i < str.length; i++){
+    if (str.charAt(i) === char){
+      count ++;
+    }
+  }
+  return count;
+}
+
+var target = document.getElementById("minimum");
+target.innerHTML = minimum(3,4);
+target.innerHTML += "<br>"+minimum(10,100);
+
+var target = document.getElementById("isEven");
+target.innerHTML = "3 is Even: "+isEven(3);
+target.innerHTML += "<br>4 is Even: "+isEven(4);
+target.innerHTML += "<br>75 is Even: "+isEven(75);
+target.innerHTML += "<br>-1 is Even: "+isEven(-1);
+target.innerHTML += "<br>-100 is Even: "+isEven(-100);
+
+var target = document.getElementById("countBs");
+target.innerHTML = countBs("BBC");
+target.innerHTML += "<br>"+countBs("bonelessBBBBBB");
+target.innerHTML += "<br>"+countBs("vmaek");
+
+var target = document.getElementById("countChar");
+target.innerHTML = countChar("BBC","C");
+target.innerHTML += "<br>"+countChar("bonelessBBBBBB","9");
+target.innerHTML += "<br>"+countChar("vmaek","k");
